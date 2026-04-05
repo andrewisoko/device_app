d93fe7bce719a4b145e884f4065c02170c759f94
andrewisoko <andrewisoko1998@gmail.com>
2026-04-05 13:19:24 +0100

    commitment 05/04

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -10,7 +10,7 @@ export class User {
     @Column( 'varchar', { length:50 , default: 'John James' })
         fullName:string;
 
-    @Column( 'varchar', { length:12 , default: 'JoJames' } )
+    @Column( 'varchar', { length:12 ,default: 'JoJames' } )
         userName:string;
 
     @Column('varchar', { length:30 , default: 'JohnJames100@email.com' })
