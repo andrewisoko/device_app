b28f6d23ae2258308ca9bf39eb89daaf30303e65
andrewisoko <andrewisoko1998@gmail.com>
2026-04-26 16:01:42 +0100

    controller for receiver inbox contract.

--- src/inbox/inbox.controller.ts
+++ src/inbox/inbox.controller.ts
@@ -1,17 +1,31 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { InboxService } from './inbox.service';
+import { Contract } from 'src/contract/entity/contract.entity';
+import { User } from 'src/user/entity/user.entity';
 
 @Controller('inbox')
 export class InboxController {
     constructor(private readonly inboxService: InboxService) {}
 
-    @Post('accept-contract')
-    async acceptContract(
-        @Body() payload: { contractId: string; receiverUsername: string },
-    ) {
-        return this.inboxService.acceptContractFromInbox(
-            payload.contractId,
-            payload.receiverUsername,
-        );
-    }
+    @Post('post-inbox')
+    postInbox(
+        @Body() dataDto:{ contract:Contract, user:User }
+    ){
+        this.inboxService.postInbox(
+            dataDto.contract,
+            dataDto.user
+        )
+    };
+
+    @Post('receiver-inbox-contract')
+        receivedContractOnInbox(
+            @Body() dataDto:{ contractId: string, receiverUsername: string, accepted:boolean }
+        ){
+            return this.inboxService.ContractReceivedOnInbox(
+                dataDto.contractId,
+                dataDto.receiverUsername,
+                dataDto.accepted,
+            )
+        }
+
 }
